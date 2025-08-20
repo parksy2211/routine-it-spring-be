99dc1373bbdb42888ab7148e70e2f9f0ad9502c4
chaanheeLEE <chanhee1519@gmail.com>
2025-08-20 16:19:09 +0900

    feat: 그룹, 그룹멤버 리포지토리 구현

--- src/main/java/com/goormi/routine/domain/group/repository/GroupMemberRepository.java
+++ src/main/java/com/goormi/routine/domain/group/repository/GroupMemberRepository.java
@@ -5,10 +5,12 @@ import com.goormi.routine.domain.group.entity.GroupMember;
 import com.goormi.routine.domain.group.entity.GroupMemberRole;
 import com.goormi.routine.domain.group.entity.GroupMemberStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
     Optional<GroupMember> findByMemberId(Long memberId);
     Optional<GroupMember> findByGroupAndRole(Group group, GroupMemberRole role);
--- src/main/java/com/goormi/routine/domain/group/repository/GroupRepository.java
+++ src/main/java/com/goormi/routine/domain/group/repository/GroupRepository.java
@@ -5,10 +5,12 @@ import com.goormi.routine.domain.group.entity.GroupMember;
 import com.goormi.routine.domain.group.entity.GroupMemberRole;
 import com.goormi.routine.domain.group.entity.GroupType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findByGroupId(Long groupId);
     List<Group> findAllByMemberId(Long memberId);
