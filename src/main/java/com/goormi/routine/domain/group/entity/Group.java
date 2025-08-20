d53f7c672a3525c469315fdb1122497f39e060aa
chaanheeLEE <chanhee1519@gmail.com>
2025-08-20 15:47:11 +0900

    feat: 그룹, 그룹멤버 DDD 설계

--- src/main/java/com/goormi/routine/domain/group/entity/Group.java
+++ src/main/java/com/goormi/routine/domain/group/entity/Group.java
@@ -2,6 +2,7 @@ package com.goormi.routine.domain.group.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,12 +22,12 @@ public class Group {
     @Column(nullable = false)
     private Long leaderId;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id")
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @OneToMany(mappedBy = "groupMembers",cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<GroupMember> groupMembers = new ArrayList<>();
+    private final List<GroupMember> groupMembers = new ArrayList<>();
 
     @Column(nullable = false)
     private String groupName;
@@ -34,25 +35,112 @@ public class Group {
     @Lob
     private String description;
 
-    @Column(length = 50)
-    private String category;
-
-    private String groupImageUrl;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private GroupType groupType;
 
-
-    private int maxMembers;
 
     private LocalTime alarmTime;
 
     @Column(length = 7)
     private String authDays; // ex) 월, 수, 금 -> 0101010 (일 - 토)
 
+
+    @Column(length = 50)
+    private String category;
+
+    private String groupImageUrl;
+
+    private int maxMembers;
+
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
 
+    @Column(nullable = false)
     private boolean isActive;
+
+
+    // 생성자
+    @Builder
+    private Group(Long leaderId, String groupName, String description,
+                  GroupType groupType, LocalTime alarmTime, String authDays,
+                  String category, String groupImageUrl, Integer maxMembers ) {
+        //basicInfo
+        this.leaderId = leaderId;
+        this.groupName = groupName;
+        this.description = description;
+        this.groupType = groupType;
+        //timeInfo
+        this.alarmTime = alarmTime;
+        this.authDays = authDays;
+        //optionalInfo
+        this.category = category;
+        this.groupImageUrl = groupImageUrl;
+        this.maxMembers = (maxMembers != null ? maxMembers : 50);
+    }
+
+    public static Group createGroup(Long leaderId, String groupName, String description, GroupType groupType) {
+        Group group = Group.builder()
+                .leaderId(leaderId)
+                .groupName(groupName)
+                .description(description)
+                .groupType(groupType)
+                .build();
+
+        // 생성 초기값은 여기서 일괄 세팅
+        group.createdAt = LocalDateTime.now();
+        group.updatedAt = group.createdAt;
+        group.isActive = true;
+
+        return group;
+    }
+
+    public void addMember(Member member) {
+        GroupMember groupMember = GroupMember.createGroupMember
+                (this, member, GroupMemberRole.MEMBER, GroupMemberStatus.PENDING);
+        groupMembers.add(groupMember);
+    }
+
+    public void addLeader(Member member) {
+        GroupMember groupLeader = GroupMember.createGroupMember
+                (this,  member, GroupMemberRole.LEADER,  GroupMemberStatus.JOINED);
+        groupMembers.add(groupLeader);
+    }
+
+    public void updateBasicInfo(String name, String description, GroupType groupType) {
+        if (name == null || name.isBlank()) {
+            throw new  IllegalArgumentException("name cannot be null or blank");
+            // throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+        this.groupName = name;
+        this.description = description;
+        this.groupType = groupType;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateTimeInfo(LocalTime alarmTime, String authDays) {
+        this.alarmTime =  alarmTime;
+        this.authDays = authDays;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateOptionalInfo(String category, String groupImageUrl, int maxMembers) {
+        this.category = category;
+        this.groupImageUrl = groupImageUrl;
+        this.maxMembers = maxMembers;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+    public void activate() {
+        this.isActive = true;
+    }
+    public void deactivate() {
+        this.isActive = false;
+    }
+
+
 }
--- src/main/java/com/goormi/routine/domain/group/entity/GroupMember.java
+++ src/main/java/com/goormi/routine/domain/group/entity/GroupMember.java
@@ -2,6 +2,7 @@ package com.goormi.routine.domain.group.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,17 +21,54 @@ public class GroupMember {
     @JoinColumn(name = "group_id", nullable = false)
     private Group group;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id", nullable = false)
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private GroupMemberStatus status;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private GroupMemberRole role;
 
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
+
+
+    @Builder
+    private GroupMember(Group group, Member member, GroupMemberRole role, GroupMemberStatus status) {
+        this.group = group;
+        this.role = role;
+        this.status = status;
+    }
+
+    public static GroupMember createGroupMember (Group group, Member member, GroupMemberRole role, GroupMemberStatus status) {
+        GroupMember groupMember = GroupMember.builder()
+                .group(group)
+                .member(member)
+                .role(role)
+                .status(status)
+                .build();
+
+        groupMember.createdAt = LocalDateTime.now();
+        groupMember.updatedAt = groupMember.createdAt;
+
+        return groupMember;
+    }
+
+    public void changeRole(GroupMemberRole role) {
+        this.role = role;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void changeStatus(GroupMemberStatus status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+
 
 }
