d2e3b7840b0be85012b6a98a5d5b0ed2031bf1a3
parksy2211 <parksy20022@gmail.com>
2025-08-26 18:52:52 +0900

    개인루틴

---
+++ src/main/java/com/goormi/routine/personal_routines/dto/PersonalRoutineResponse.java
@@ -0,0 +1,25 @@
+package com.goormi.routine.personal_routines.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class PersonalRoutineResponse {
+    private Integer routineId;
+    private Integer userId;
+    private String routineName;
+    private String description;
+    private LocalTime startTime;
+    private String repeatDays;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Boolean isAlarmOn;
+    private Boolean isPublic;
+    private Boolean isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
