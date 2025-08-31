6c43f451e01f15f3c17da8b86fdde74ee86a5f68
choechang-yeol <duf325@naver.com>
2025-08-31 22:25:08 +0900

    fix: cors allowed origin에 5173포트 추가

--- src/main/java/com/goormi/routine/config/SecurityConfig.java
+++ src/main/java/com/goormi/routine/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
