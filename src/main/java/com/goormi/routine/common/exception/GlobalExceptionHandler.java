a3206fa22b5fb8df1c2f9bf70e8b0cdd69010ffb
chaanheeLEE <chanhee1519@gmail.com>
2025-09-09 19:14:24 +0900

    test

--- src/main/java/com/goormi/routine/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/goormi/routine/common/exception/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.stream.Collectors;
 
 @Slf4j
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
