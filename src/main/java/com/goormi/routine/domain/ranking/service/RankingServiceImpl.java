538995768467d8e10ca7b810e343de1d18e64ae9
chaanheeLEE <chanhee1519@gmail.com>
2025-09-16 19:19:04 +0900

    rollback

--- src/main/java/com/goormi/routine/domain/ranking/service/RankingServiceImpl.java
+++ src/main/java/com/goormi/routine/domain/ranking/service/RankingServiceImpl.java
@@ -102,7 +102,6 @@ public class RankingServiceImpl implements RankingService {
 
 			if (scoreData.getGroup() != null) {
 				Group group = scoreData.getGroup();
-                if (!group.isActive()) break;
 
 				int memberCount = groupMemberRepository.countMembersByGroupId(groupId);
 				int activeMembers = groupMemberRepository.countActiveByGroupId(groupId, monthYear);
